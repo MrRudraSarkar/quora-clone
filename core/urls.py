3701b451b7a625a853ec9b6f5a906fbe7f93f0f2
Rudrasish Sarkar <sarkarrudra15@gmail.com>
2025-04-09 01:18:16 +0530

    Fixed a bug where 'urlpatterns' was written as 'url_patterns' in core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-url_patterns = [
-    path('', views.question_list, name="question_list"),
-    
+urlpatterns = [
+    path('', views.question_list, name="question_list")
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Question, Answer
+from .models import Question
 
 # Create your views here.
 def question_list(request):
