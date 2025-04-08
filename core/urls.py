51569f9735ef61101a8d9f936db6419b5fda4056
Rudrasish Sarkar <sarkarrudra15@gmail.com>
2025-04-09 01:41:05 +0530

    Added creation functionality with form, view and template

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.question_list, name="question_list")
+    path('', views.question_list, name="question_list"),
+    path('ask/', views.question_create, name='question_create')
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .models import Question
+from .forms import QuestionForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def question_list(request):
     questions = Question.objects.all().order_by('-created_at')
     return render(request, "core/question_list.html", {"questions": questions})
 
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('question_list')
+    else:
+        form = QuestionForm()
+    return render(request, 'core/question_form.html', {'form': form})
