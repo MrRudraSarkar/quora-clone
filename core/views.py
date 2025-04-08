315fa96fe3dbe5e5c690f25ae1a0e10afbbdea4a
Rudrasish Sarkar <sarkarrudra15@gmail.com>
2025-04-09 01:06:53 +0530

    Set up views for core app and set the url patterns as well

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Question, Answer
 
 # Create your views here.
+def question_list(request):
+    questions = Question.objects.all().order_by('-created_at')
+    return render(request, "core/question_list.html", {"questions": questions})
+
