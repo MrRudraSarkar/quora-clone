58d74295f53532201eb9f6a486a2f60503063d39
Rudrasish Sarkar <sarkarrudra15@gmail.com>
2025-04-10 21:04:33 +0530

    Added views and URL routing for adding questions and answers.

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.question_list, name="question_list"),
-    path('ask/', views.question_create, name='question_create')
+    path('ask/', views.post_question, name='post_question'),
+    path('question/<int:question_id>/', views.question_detail, name='question_detail'),
+    path('question/<int:question>/answer', views.post_answer, name='post_answer')
 ]
--- core/views.py
+++ core/views.py
@@ -1,19 +1,47 @@
 from django.shortcuts import render
 from .models import Question
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def question_list(request):
     questions = Question.objects.all().order_by('-created_at')
     return render(request, "core/question_list.html", {"questions": questions})
 
-def question_create(request):
+@login_required
+def post_question(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
-            form.save()
+            question = form.save(commit=False)
+            question.author = request.user
+            question.save()
             return redirect('question_list')
     else:
         form = QuestionForm()
     return render(request, 'core/question_form.html', {'form': form})
+
+def question_detail(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    answers = question.answers.all()
+    form = AnswerForm()
+    return render(request, 'core/question_detail.html', {
+        'question': question,
+        'answers': answers,
+        'form': form
+    })
+
+@login_required
+def post_answer(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.isValid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.question = question
+            answer.save()
+    
+    return redirect('question_detail', question_id = question_id)
